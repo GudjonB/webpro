4be5f6021cec514bb77b0bc0b5ef4dbcb07a8276
Gudjon <gudjon17@ru.com>
2019-02-26 00:45:43 +0000

    blabla

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
-var game = {boxes:"", _id: "", difficulty: "", dateGenerated: ""};
+var game = {board:{boxes:""}, _id: "", difficulty: "", dateGenerated: ""};
 
 function getSudoku(setting) {
 
     //The URL to which we will send the request
-    var url = 'https://veff213-sudoku.herokuapp.com/api/v1/sudoku';
+    var url = 'https://veff213-sudoku.herokuapp.com/api/v1/sudok';
 
     //Perform an AJAX POST request to the url, and set the param 'myParam' in the request body to paramValue
     axios.post(url, { difficulty: setting })
@@ -17,18 +17,18 @@ function getSudoku(setting) {
         //When unsuccessful, print the error.
         console.log(error);
         if (setting == 'easy') {
-            game.boxes = [[5,6,4,'.','.',3,2,'.',1],[8,7,2,'.',1,'.',3,9,'.'],[3,9,1,'.','.','.','.','.',5],
+            game.board.boxes = [[5,6,4,'.','.',3,2,'.',1],[8,7,2,'.',1,'.',3,9,'.'],[3,9,1,'.','.','.','.','.',5],
                         [4,2,9,6,5,7,3,1,8],['.','.',8,2,3,1,9,4,7],[7,1,3,8,4,9,5,2,6],
                         ['.','.',6,'.',3,5,8,4,2],[4,2,3,7,8,9,1,'.','.'],['.',5,8,2,6,4,9,3,7]];
             game._id = "default-easy";
         } else if (setting = 'medium') {
-            game.boxes = [[8,7,'.','.',4,'.',6,2,5],[4,5,'.','.',2,'.','.',1,'.'],[2,1,'.',8,5,'.','.',9,'.'],
+            game.board.boxes = [[8,7,'.','.',4,'.',6,2,5],[4,5,'.','.',2,'.','.',1,'.'],[2,1,'.',8,5,'.','.',9,'.'],
                           [7,6,'.',5,'.',4,'.',8,'.'],[9,3,1,8,6,2,5,'.',7],[5,4,8,3,'.',1,9,6,2],
                           [2,'.',7,9,5,8,4,'.',6],['.',9,4,6,7,3,2,'.',5],['.','.',5,1,'.',4,'.','.','.']];
             game._id = "default-medium";
             
         } else if (setting = 'hard') {
-            game.boxes = [[4,'.','.',9,'.','.','.','.','.'],['.','.','.','.',4,'.','.','.','.'],[5,3,9,6,'.',1,7,'.',4],
+            game.board.boxes = [[4,'.','.',9,'.','.','.','.','.'],['.','.','.','.',4,'.','.','.','.'],[5,3,9,6,'.',1,7,'.',4],
                           ['.',9,6,'.',4,7,'.','.','.'],['.',7,8,5,'.',2,1,9,6],[2,5,3,9,1,6,8,4,7],
                           ['.','.',1,'.',8,4,2,'.','.'],['.',8,'.','.','.','.','.',5,4],[4,'.',2,3,'.',5,1,7,8]];
             game._id = "default-hard";
@@ -62,9 +62,9 @@ function getSudoku(setting) {
                             document.getElementById((String)(i)+" Box").appendChild(newinputbox);
                         }
                 }
-                document.getElementById("sudokuBoard").appendChild(br);
+                
             }
-            document.getElementById("sudokuBoard").appendChild(br);
+           // document.getElementById("sudokuBoard").appendChild(br);
         }
     });
 }  
